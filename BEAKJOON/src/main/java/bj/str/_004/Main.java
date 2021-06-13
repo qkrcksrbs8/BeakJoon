e60520f433b8a83b3295d7e740bc4ee12c6d8fe0
ParkChanGyun <qkrcksrbs8@naver.com>
2021-06-14 00:30:26 +0900

    샘플

---
+++ BEAKJOON/src/main/java/bj/str/_004/Main.java
@@ -0,0 +1,32 @@
+package bj.str._004;
+
+ 
+public class Main {
+	
+	public static void main(String[] args) {
+		solution(3,12);
+	}
+	
+	 public static int[] solution(int n, int m) {
+	        return gdc(n,m);
+	    }
+	    
+	    public static int[] gdc(int n, int m){
+	        int o = n;    
+	        int p = m;
+	        while (m != 0){
+	            int l =  n % m;
+	            n = m;
+	            m = l;
+	        }
+	        o = o*p/n;
+	        int[] answer = new int[2];
+	        answer[0] = n;
+	        answer[1] = o;
+	        System.out.println(n);
+	        System.out.println(o);
+	        return answer;
+	    }
+	    
+}
+ 
