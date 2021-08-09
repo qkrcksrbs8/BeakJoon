28b4f1aeddc8520eb6a0ac0428037d30d7822f7e
ParkChanGyun <qkrcksrbs8@naver.com>
2021-08-09 23:03:37 +0900

    2164 수정

--- BEAKJOON/src/main/java/bj/queue/_2164/Main.java
+++ BEAKJOON/src/main/java/bj/queue/_2164/Main.java
@@ -14,17 +14,19 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		Deque<Integer> q 	= new LinkedList<>();
+		
 		for (int i = 1; i <= N; ++i) {
-			q.push(N-i+1);
+			q.push(N-i+1);			//입력 받은 숫자만큼 queue에 삽입
 		}
+		
 		for (int i = 0; i < N-1; ++i) {
-			q.poll();
-			q.addLast(q.poll());
+			q.poll();				//제일 위에 있는 숫자 제거
+			q.addLast(q.poll());	//제일 위에 있는 숫자 마지막으로 이동
 		}
-		System.out.println(q.poll());
+		
+		System.out.println(q.poll());//결과
 	}
 }
