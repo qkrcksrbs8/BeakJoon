cbb7d77aae30ba91072b88a512f291e8b8a72080
ParkChanGyun <qkrcksrbs8@naver.com>
2021-08-12 00:47:07 +0900

    럭키 스트레이트

---
+++ BEAKJOON/src/main/java/bj/str/_18406/Main.java
@@ -0,0 +1,28 @@
+package bj.str._18406;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	/**
+	 * 럭키 스트레이트
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str 	= br.readLine();
+		int count 	= str.length();
+		int left	= 0;
+		int right	= 0;
+		for (int i = 0; i < count/2; ++i) {
+			left += (str.charAt(i)-0);
+		}
+		for (int i = count/2; i < count; ++i) {
+			right += (str.charAt(i)-0);
+		}
+		System.out.println((0 == (left-right)? "LUCKY":"READY"));
+	}
+}
